0e2d5b7df62784a96ff810a060c8cdabcaf0ac33
natswatch <nathaliec@me.com>
2020-08-18 13:37:38 -0700

    tasks array

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 
-
+var tasks = [];
 var taskIdCounter = 0;
 
 // form values are defined a
@@ -38,7 +38,8 @@ var taskFormHandler = function() {
         // package up data as an object
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do"
         };
 
         // send it as an argument to createTaskEl
@@ -54,6 +55,12 @@ var completeEditTask = function(taskName, taskType, taskId) {
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
+    for (var i=0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    }
     alert("Task Updated!");
 };
 
@@ -74,11 +81,16 @@ var createTaskEl = function(taskDataObj) {
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);    
-
+    taskDataObj.id = taskIdCounter;
+    tasks.push(taskDataObj);
+    
     tasksToDoEl.appendChild(listItemEl);
 
     // increase task counter for next unique id
     taskIdCounter++;
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
     
 };
 
@@ -181,6 +193,13 @@ var taskStatusChangeHandler = function(event) {
     else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+
+    // update task's in tasks array
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+          tasks[i].status = statusValue;
+        }
+    }
 };
 
 var dragTaskHandler = function() {
@@ -221,6 +240,12 @@ var dropTaskHandler = function(event) {
     dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 
+    // loop through tasks array to find and update the updated task's status
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(id)) {
+        tasks[i].status = statusSelectEl.value.toLowerCase();
+        }
+    }
 }; 
 
 var dragLeaveHandler = function(event) {
